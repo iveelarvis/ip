8e952dc7046df6823f6fe3fbbc0230bb4b03f045
iveel <iveel.arviss@gmail.com>
2026-08-24 22:53:54 +0800

    Add echo functionality.

--- src/main/java/Farz.java
+++ src/main/java/Farz.java
@@ -1,19 +1,40 @@
+import java.util.Scanner;
+
 /**
  * Runs the Farz chatbot application.
  */
 public class Farz {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String banner = " _____              \n"
                 + "|  ___|_ _ _ __ ____\n"
                 + "| |_ / _` | '__|_  /\n"
                 + "|  _| (_| | |   / / \n"
                 + "|_|  \\__,_|_|  /___|\n";
-	System.out.println("____________________________________________________________");
-	System.out.println(banner);
-	System.out.println("Hello! I'm Farz.");
-	System.out.println("What can I do for you?");
-	System.out.println("____________________________________________________________");
-	System.out.println("Bye. Hope to see you again soon!");
-	System.out.println("____________________________________________________________");
+
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello! I'm Farz.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+
+            if (input.equals("bye")) {
+                break;
+            }
+            echoCommand(input);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void echoCommand(String input) {
+        System.out.println(input);
+        System.out.println("____________________________________________________________");
     }
 }
