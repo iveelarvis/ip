06053b46d4d0ac035c6b0018dedcb49f5b6879cc
iveel <iveel.arviss@gmail.com>
2026-08-24 22:18:32 +0800

    Change name to Farz

---
+++ src/main/java/Farz.java
@@ -0,0 +1,13 @@
+/**
+ * Runs the Farz chatbot application.
+ */
+public class Farz {
+    public static void main(String[] args) {
+        String banner = " _____              \n"
+                + "|  ___|_ _ _ __ ____\n"
+                + "| |_ / _` | '__|_  /\n"
+                + "|  _| (_| | |   / / \n"
+                + "|_|  \\__,_|_|  /___|\n";
+        System.out.println(banner);
+    }
+}
