949d345d997ba4328ab4041b0cea0aa1d0a9465b
iveel <iveel.arviss@gmail.com>
2026-08-25 20:58:37 +0800

    Refractored for better structure

--- src/main/java/Farz.java
+++ src/main/java/Farz.java
@@ -4,45 +4,66 @@ import java.util.Scanner;
  * Runs the Farz chatbot application.
  */
 public class Farz {
-    private static final String[] TASKS = new String[100];
+    private static final String DIVIDER = "____________________________________________________________";
+    private static final String BANNER = " _____              \n"
+            + "|  ___|_ _ _ __ ____\n"
+            + "| |_ / _` | '__|_  /\n"
+            + "|  _| (_| | |   / / \n"
+            + "|_|  \\__,_|_|  /___|\n";
+    private static final Task[] TASKS = new Task[100];
     private static int taskCount = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String banner = " _____              \n"
-                + "|  ___|_ _ _ __ ____\n"
-                + "| |_ / _` | '__|_  /\n"
-                + "|  _| (_| | |   / / \n"
-                + "|_|  \\__,_|_|  /___|\n";
-
-        System.out.println("____________________________________________________________");
-        System.out.println(banner);
+        printDivider();
+        System.out.println(BANNER);
         System.out.println("Hello! I'm Farz.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        printDivider();
 
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            System.out.println("____________________________________________________________");
-
-            if (input.equals("bye")) {
-                break;
-            }
-            handleCommand(input);
+        boolean shouldContinue = true;
+        while (shouldContinue && scanner.hasNextLine()) {
+            String rawInput = scanner.nextLine().trim();
+            shouldContinue = executeCommand(rawInput);
         }
 
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        printDivider();
     }
 
-    private static void handleCommand(String input) {
-        if (input.equals("list")) {
-            listTasks();
-        } else {
-            addTask(input);
+    /**
+     * Executes a command entered by the user and indicates whether to accept more commands.
+     *
+     * @param input Full user input, including any command arguments.
+     * @return {@code false} when the user enters {@code bye}; {@code true} otherwise.
+     */
+    private static boolean executeCommand(String input) {
+        String[] commandParts = input.split("\\s+", 2);
+        switch (commandParts[0]) {
+        case "bye" -> {
+            return false;
         }
-        System.out.println("____________________________________________________________");
+        case "list" -> listTasks();
+        case "mark" -> updateTaskStatus(commandParts[1], true);
+        case "unmark" -> updateTaskStatus(commandParts[1], false);
+        default -> addTask(input);
+        }
+        printDivider();
+        return true;
+    }
+
+    private static void updateTaskStatus(String taskNumber, boolean isDone) {
+        int taskIndex = Integer.parseInt(taskNumber) - 1;
+        Task task = TASKS[taskIndex];
+        if (isDone) {
+            task.markAsDone();
+            System.out.println("Nice! I've marked this task as done:");
+        } else {
+            task.markAsNotDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+        System.out.println("  " + task);
     }
 
     private static void listTasks() {
@@ -52,8 +73,12 @@ public class Farz {
     }
 
     private static void addTask(String input) {
-        TASKS[taskCount] = input;
+        TASKS[taskCount] = new Task(input);
         taskCount++;
         System.out.println("added: " + input);
+    }
+
+    private static void printDivider() {
+        System.out.println(DIVIDER);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,45 @@
+/**
+ * Represents a task that can be marked as done or not done.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Creates a task with the given description.
+     *
+     * @param description Description of the task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns an icon indicating whether this task is done.
+     *
+     * @return {@code "X"} if done, or a space otherwise.
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /**
+     * Marks this task as done.
+     */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Marks this task as not done.
+     */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
